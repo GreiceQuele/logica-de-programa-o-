602afbbd5fec1306d589544c6a2d9ce9d78424d3
Quele <greice.caetano@gmail.com>
2024-03-06 15:25:02 -0300

    aula sobre listas e sua utilização no Javascript. como também criado arquivo desafio de listas

---
+++ desafioListas.js
@@ -0,0 +1,10 @@
+let listaGenerica = [];
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin' ,'Python'];
+linguagensDeProgramacao.push['Java','C','C++','Kotlin','Python'];
+
+nomes = ['Maria', 'João', 'Leticia'];
+console.log(nomes[0]);
+
+nomes = ['Maria', 'João', 'Leticia'];
+console.log(nomes[2]);
+
